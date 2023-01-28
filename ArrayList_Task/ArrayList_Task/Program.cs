8e33c3192f12b7eb49eaa4b2fc6c68696aaad846
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-01-29 02:29:16 +0500

    Add files via upload

---
+++ ArrayList_Task/ArrayList_Task/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals giraffe = new Animals();
+            giraffe.Name = "Giraffe";
+            Animals lion    = new Animals();
+            lion.Name = "Lion"; 
+            Animals tiger   = new Animals();
+            tiger.Name = "Tiger";
+
+            Trainers mirza  = new Trainers();
+            mirza.TrainerName = "Mirza Arslan";
+
+            ArrayList al = new ArrayList();
+            al.Add(giraffe);
+            al.Add(lion);
+            al.Add(tiger);
+
+            al.Add(mirza);
+
+            foreach (var item in al)
+            {
+                // for getting two different methods of two different instances
+
+                if (typeof(Animals) == item.GetType())
+                {
+                    ((Animals)item).SayHi();
+                    ((Animals)item).Feed();
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    ((Trainers)item).SayHi();
+                }
+            }
+        }
+    }
+
+    #region Struct 01
+
+    struct Animals 
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void SayHi() 
+        {
+            Console.WriteLine("Hi my name is : "+name);
+        }
+        public void Feed() 
+        {
+            Console.WriteLine(name + " is eating");
+
+        }
+
+    }
+    #endregion
+
+    #region Struct 02
+    struct Trainers 
+    {
+        private string trainerName;
+
+        public string TrainerName
+        {
+            get { return trainerName; }
+            set { trainerName = value; }
+        }
+        public void SayHi() 
+        {
+            Console.WriteLine("Hi from trainer : " + trainerName);
+        }
+
+    }
+    #endregion
+}
