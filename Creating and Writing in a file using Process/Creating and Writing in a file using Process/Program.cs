503d5440a8335df355982e5f385b32c12fcc3dcf
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-02-08 03:32:59 +0500

    Add files via upload

---
+++ Creating and Writing in a file using Process/Creating and Writing in a file using Process/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Creating_and_Writing_in_a_file_using_Process
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@"F:\\C#Projects\notepadeExerciceUsingProcess.txt","I am writing this dummy text using Process");
+            System.Diagnostics.Process.Start(@"F:\\C#Projects\notepadeExerciceUsingProcess.txt");
+        }
+    }
+}
