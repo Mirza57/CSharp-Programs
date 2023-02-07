12a69f9e726e3999b564f0f80fd2b5ec520aaeba
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-02-08 02:19:07 +0500

    Add files via upload

---
+++ Recursive Function Program/Recursive Function Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Function_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            Console.WriteLine($"Input is equal to : {x}");
+
+            Console.WriteLine("Output of the factorial will be : " + RecursiveFunction(x));
+            var player = (7, "Mirza", 7.8);
+            Console.WriteLine(player);
+
+
+        }
+        public static int RecursiveFunction(int input) 
+        {
+            if (input == 0)
+            return 1;
+            
+            return input * RecursiveFunction(input - 1);
+
+           
+        }
+    }
+}
