97032f924ea6d6733d06989e53fdd0f9b3ac0d3e
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-02-09 03:45:32 +0500

    Add files via upload

---
+++ Yield Keyword Program/Yeild Keyword Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield_Keyword_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in MathOperations(3,9)) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        /*
+         *  Yield Keyword is so powerful.
+         *  We can change the view of program
+         *  we can return more then one thing using Yield Keyword
+         */
+        public static IEnumerable<double> MathOperations(int num1, int num2)
+        {
+           yield return num1 + num2;
+           yield return num1 * num2;
+           yield return num1 - num2;
+           yield return Math.Round((double)num1 / (double)num2, 2);
+
+        }
+    }
+}
